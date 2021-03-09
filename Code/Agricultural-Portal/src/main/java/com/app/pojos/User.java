0e99e65352bcefc811079f315152874fc66af95b
vikas-patel-06 <3471+vikas-patel-06@users.noreply.github.com>
2021-03-09 23:37:02 +0530

    User pojo updated

--- Code/Agricultural-Portal/src/main/java/com/app/pojos/User.java
+++ Code/Agricultural-Portal/src/main/java/com/app/pojos/User.java
@@ -4,78 +4,87 @@ import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
+
 
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "users")
 public class User {
-//	Id	FName	LName	Email	MobileNo.	Password	PostalCode	DateRegistered	Role
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer userId;
 	
-	@Column(length = 20)
-	@NotBlank(message = "required")
+	@Column(length = 30)
+	@NotBlank(message = "Required")
 	private String firstName;
 	
-	@Column(length = 20)
-	@NotBlank(message = "required")	
+	@Column(length = 30)
+	@NotBlank(message = "Required")
 	private String lastName;
 	
-	@Column(length = 50,unique = true)
+	@Column(length = 50)
+	@NotBlank(message = "Required")
+	private String bussinessName;
+	
+	@Column(length = 50)
+	@NotBlank(message = "Required")
 	@Email(message = "Enter valid email")
-	@NotBlank(message = "required")
 	private String email;
 	
-	@Column(length = 15,unique = true)
+	@Column(length = 15)
 	private String mobileNo;
 	
+	@Column(length = 20, nullable = false)
 	@Pattern(regexp="((?=.\\d)(?=.[a-z])(?=.[#@$]).{5,20})",message = "Invalid password!")
-	@NotBlank(message = "required")
 	private String password;
 	
-	@NotBlank(message = "required")
-	@Column(length = 10)
-	private String pin;
+	@Column(length = 20)
+	@Pattern(regexp="^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$",message = "Invalid GSTIN !")
+	private String gstin;
 	
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate regDate;
 	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "selectedUser", orphanRemoval = true)
+	private List<Address> addresses;
+	
 	@Enumerated(EnumType.STRING)
 	private Role role;
 	
-	@OneToMany(cascade = CascadeType.ALL,mappedBy ="selectedUser",orphanRemoval = true)
-	private List<Address> address;
+	/*
+	 * @OneToMany(cascade = CascadeType.ALL, mappedBy = "selectedUser",
+	 * orphanRemoval = true) private List<BankDetails> bankDetails;
+	 * 
+	 * @OneToMany(cascade = CascadeType.ALL, mappedBy = "soldBySeller",
+	 * orphanRemoval = true) private List<Product> products;
+	 */
 	
-	@OneToMany(cascade = CascadeType.ALL,mappedBy ="selectedUser",orphanRemoval = true)
-	private List<BankDetails> bankDetails;
 	public User() {
 	}
 
-	public User(Integer id, String firstName, String lastName, String email, String mobileNumber, String password,
-			String pin, LocalDate regDate, Role role) {
+	public User(Integer sellerId, String firstName, String lastName, String bussinessName, String email,
+			String mobileNo, String password, String gstin, LocalDate regDate) {
 		super();
-		this.userId = id;
+		this.userId = sellerId;
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.bussinessName = bussinessName;
 		this.email = email;
-		this.mobileNo = mobileNumber;
+		this.mobileNo = mobileNo;
 		this.password = password;
-		this.pin = pin;
+		this.gstin = gstin;
 		this.regDate = regDate;
-		this.role = role;
 	}
 
-	public Integer getId() {
+	public Integer getUserId() {
 		return userId;
 	}
 
-	public void setId(Integer id) {
-		this.userId = id;
+	public void setUserId(Integer userId) {
+		this.userId = userId;
 	}
 
 	public String getFirstName() {
@@ -94,6 +103,14 @@ public class User {
 		this.lastName = lastName;
 	}
 
+	public String getBussinessName() {
+		return bussinessName;
+	}
+
+	public void setBussinessName(String bussinessName) {
+		this.bussinessName = bussinessName;
+	}
+
 	public String getEmail() {
 		return email;
 	}
@@ -102,12 +119,12 @@ public class User {
 		this.email = email;
 	}
 
-	public String getMobileNumber() {
+	public String getMobileNo() {
 		return mobileNo;
 	}
 
-	public void setMobileNumber(String mobileNumber) {
-		this.mobileNo = mobileNumber;
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
 	}
 
 	public String getPassword() {
@@ -118,12 +135,12 @@ public class User {
 		this.password = password;
 	}
 
-	public String getPin() {
-		return pin;
+	public String getGstin() {
+		return gstin;
 	}
 
-	public void setPin(String pin) {
-		this.pin = pin;
+	public void setGstin(String gstin) {
+		this.gstin = gstin;
 	}
 
 	public LocalDate getRegDate() {
@@ -134,6 +151,14 @@ public class User {
 		this.regDate = regDate;
 	}
 
+	public List<Address> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(List<Address> addresses) {
+		this.addresses = addresses;
+	}
+
 	public Role getRole() {
 		return role;
 	}
@@ -142,10 +167,7 @@ public class User {
 		this.role = role;
 	}
 
-	@Override
-	public String toString() {
-		return "User [id=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
-				+ ", mobileNumber=" + mobileNo + ", pin=" + pin + ", regDate=" + regDate + ", role=" + role + "]";
-	}
-
+	
+	
+	
 }
