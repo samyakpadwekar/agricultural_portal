dad3253d1d4363bc3dd6944bd151fce09c9e4564
@43063_chinmay <chinmay.ohol@gmail.com>
2021-03-09 23:03:38 +0530

    Seller annotations added

--- Code/Agricultural-Portal/src/main/java/com/app/pojos/Seller.java
+++ Code/Agricultural-Portal/src/main/java/com/app/pojos/Seller.java
@@ -1,25 +1,76 @@
 package com.app.pojos;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "sellers")
 public class Seller {
-//	 Id FName LName Bussiness Name Email
-//	 MobileNo.	Password	ValidationNumber(GSTIN)	DateRegistered
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer sellerId;
+	
+	@Column(length = 30)
+	@NotBlank(message = "Required")
 	private String firstName;
+	
+	@Column(length = 30)
+	@NotBlank(message = "Required")
 	private String lastName;
+	
+	@Column(length = 50)
+	@NotBlank(message = "Required")
 	private String bussinessName;
+	
+	@Column(length = 50)
+	@NotNull(message = "Required")
+	@Email(message = "Enter valid email")
 	private String email;
+	
+	@Column(length = 15)
 	private String mobileNo;
+	
+	@Column(length = 20, nullable = false)
+	@Pattern(regexp="((?=.*\\d)(?=.*[a-z])(?=.*[#@$*]).{5,20})",message = "Invalid password!")
 	private String password;
-	private String validationNo;
+	
+	@Column(length = 20)
+	@Pattern(regexp="^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$",message = "Invalid password!")
+	private String gstin;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate regDate;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "selectedUser", orphanRemoval = true)
+	private List<Address> addresses;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "selectedUser", orphanRemoval = true)
+	private List<BankDetails> bankDetails;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "soldBySeller", orphanRemoval = true)
+	private List<Product> products;
 	
 	public Seller() {
 	}
 
 	public Seller(Integer sellerId, String firstName, String lastName, String bussinessName, String email,
-			String mobileNo, String password, String validationNo, LocalDate regDate) {
+			String mobileNo, String password, String gstin, LocalDate regDate) {
 		super();
 		this.sellerId = sellerId;
 		this.firstName = firstName;
@@ -28,7 +79,7 @@ public class Seller {
 		this.email = email;
 		this.mobileNo = mobileNo;
 		this.password = password;
-		this.validationNo = validationNo;
+		this.gstin = gstin;
 		this.regDate = regDate;
 	}
 
@@ -88,12 +139,12 @@ public class Seller {
 		this.password = password;
 	}
 
-	public String getValidationNo() {
-		return validationNo;
+	public String getGstin() {
+		return gstin;
 	}
 
-	public void setValidationNo(String validationNo) {
-		this.validationNo = validationNo;
+	public void setGstin(String gstin) {
+		this.gstin = gstin;
 	}
 
 	public LocalDate getRegDate() {
