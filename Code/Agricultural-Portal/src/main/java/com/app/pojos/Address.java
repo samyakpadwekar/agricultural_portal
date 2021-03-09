485d38e3081139e05c9099be75358ac0de25b9b8
vikas-patel-06 <3471+vikas-patel-06@users.noreply.github.com>
2021-03-09 23:05:52 +0530

    Annotations added in address pojo

--- Code/Agricultural-Portal/src/main/java/com/app/pojos/Address.java
+++ Code/Agricultural-Portal/src/main/java/com/app/pojos/Address.java
@@ -1,26 +1,65 @@
 package com.app.pojos;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "addresses")
 public class Address {
 //	UserId	FullName	MobileNo	PinCode 	AddLine1	Add line2	
 	// Landmark City State Country(India)
-	private Integer userId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer addressId;
+
+	@Column(length = 20, nullable = false)
+	@NotBlank(message = "required")
 	private String fullName;
+
+	@Column(length = 20, nullable = false)
+	@NotBlank(message = "required")
 	private String mobileNumber;
+
+	@Column(length = 10)
+	@NotBlank(message = "required")
 	private String pinCode;
+
+	@Column(length = 100)
+	@NotBlank(message = "required")
 	private String line1;
+
+	@Column(length = 100)
+	@NotBlank(message = "required")
 	private String line2;
+
+	@Column(length = 100)
+	@NotBlank(message = "required")
 	private String landMark;
+
+	@Column(length = 25)
+	@NotBlank(message = "required")
 	private String city;
+
+	@Column(length = 25)
+	@NotBlank(message = "required")
 	private String state;
+
+	@Column(length = 25)
+	@NotBlank(message = "required")
 	private String country;
+
+	@ManyToOne
+	@JoinColumns({ @JoinColumn(name = "user_id", referencedColumnName = "user_id"),
+			@JoinColumn(name = "user_id", referencedColumnName = "seller_id") })
+	private Object selectedUser;
 
 	public Address() {
 	}
 
-	public Address(Integer userId, String fullName, String mobileNumber, String pinCode, String line1, String line2,
+	public Address(Integer addressId, String fullName, String mobileNumber, String pinCode, String line1, String line2,
 			String landMark, String city, String state, String country) {
 		super();
-		this.userId = userId;
+		this.addressId = addressId;
 		this.fullName = fullName;
 		this.mobileNumber = mobileNumber;
 		this.pinCode = pinCode;
@@ -33,11 +72,11 @@ public class Address {
 	}
 
 	public Integer getUserId() {
-		return userId;
+		return addressId;
 	}
 
 	public void setUserId(Integer userId) {
-		this.userId = userId;
+		this.addressId = userId;
 	}
 
 	public String getFullName() {
@@ -111,6 +150,5 @@ public class Address {
 	public void setCountry(String country) {
 		this.country = country;
 	}
-	
-	
+
 }
