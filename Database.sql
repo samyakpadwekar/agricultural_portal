a4aa00bf6df37088e0848bae894f1ef1bf05281f
Pankaj Chavan <chavanpankaj1996@gmail.com>
2021-03-10 10:41:28 +0530

    database file updated

--- Database.sql
+++ Database.sql
@@ -4,4 +4,4 @@ GRANT ALL PRIVILEGES ON project.* TO 'agri'@'localhost';
 
 FLUSH PRIVILEGES;
 
-INSERT INTO users VALUES(default,null,admin@gmail.com,'Admin',null,'admin','+917894561230','abc#1234','2018-12-01','ADMIN',0);
+INSERT INTO users VALUES(default,null,'admin@gmail.com','Admin',null,'admin','+917894561230','abc#1234','2018-12-01','ADMIN',default);
