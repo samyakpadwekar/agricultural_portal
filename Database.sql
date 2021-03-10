df99104f97f4c27a9555169f29d095e360c29a85
Pankaj Chavan <chavanpankaj1996@gmail.com>
2021-03-10 10:35:55 +0530

    admin query added

--- Database.sql
+++ Database.sql
@@ -3,3 +3,5 @@ CREATE USER 'agri'@'localhost' IDENTIFIED BY 'manager';
 GRANT ALL PRIVILEGES ON project.* TO 'agri'@'localhost';
 
 FLUSH PRIVILEGES;
+
+INSERT INTO users VALUES(default,null,admin@gmail.com,'Admin',null,'admin','+917894561230','abc#1234','2018-12-01','ADMIN',0);
