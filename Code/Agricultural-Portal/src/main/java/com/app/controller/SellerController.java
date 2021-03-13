32e9fb7f6de2b8edce0ea6b4e766c213e9f2fc14
vikas-patel-06 <3471+vikas-patel-06@users.noreply.github.com>
2021-03-13 09:03:50 +0530

    SellerCotnroller bug Fixed

--- Code/Agricultural-Portal/src/main/java/com/app/controller/SellerController.java
+++ Code/Agricultural-Portal/src/main/java/com/app/controller/SellerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -69,7 +70,7 @@ public class SellerController {
 		return ResponseEntity.ok(sellerService.deleteProduct(productId));
 	}
 
-	@PostMapping("/update-profile")
+	@PutMapping("/update-profile")
 	public ResponseEntity<?> editProfile(@RequestBody SellerDTO s) {
 		System.out.println("In edit profile of Seller" + s);
 		return ResponseEntity.ok(sellerService.editProfile(s));
