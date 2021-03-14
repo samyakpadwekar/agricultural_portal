7c8646e691c3a9427da18ad9a2d192a4618dfc76
43063_chinmay <chinmay.ohol@gmail.com>
2021-03-14 23:44:20 +0530

    get seller's products query updated for lazy fetch

--- Code/Agricultural-Portal/src/main/java/com/app/dao/ProductRepository.java
+++ Code/Agricultural-Portal/src/main/java/com/app/dao/ProductRepository.java
@@ -13,6 +13,6 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 	@Query("select p from Product p left outer join fetch p.category where p.category.catName=:nm")
 	List<Product> findByCategoryName(@Param("nm") String categoryName);
    
-   	@Query("select p from Product p left outer join fetch p.seller where p.seller.sellerId=:slid")
+   	@Query("select p from Product p left outer join fetch p.seller join fetch p.category join fetch p.productCatalogue where p.seller.sellerId=:slid")
 	List<Product> findBySellerId(@Param("slid") Integer sellerId);
 }
