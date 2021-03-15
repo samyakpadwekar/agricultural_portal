c35077d64b41a8e52055eb0999e943d287224bc8
vikas-patel-06 <3471+vikas-patel-06@users.noreply.github.com>
2021-03-15 23:30:01 +0530

    JwtAuthenticationEntryPoint created by : VIKAS + PANKAJ

---
+++ Code/Agricultural-Portal/src/main/java/com/app/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package com.app.config;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+	private static final Logger logger = LoggerFactory.getLogger(AuthenticationEntryPoint.class);
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException {
+		logger.error("Unauthorized error: {}", authException.getMessage());
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+	}
+}
