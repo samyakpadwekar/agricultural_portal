72b8f1edfc3e0ba54d07a1770db2c9b37afa8bc0
vikas-patel-06 <3471+vikas-patel-06@users.noreply.github.com>
2021-03-16 00:05:46 +0530

    Crossorgin Annotation added

--- Code/Agricultural-Portal/src/main/java/com/app/controller/SellerController.java
+++ Code/Agricultural-Portal/src/main/java/com/app/controller/SellerController.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +34,7 @@ import com.app.service.ISellerService;
 
 @RestController
 @RequestMapping("/seller")
+@CrossOrigin
 public class SellerController {
 
 	@Autowired
--- Code/Agricultural-Portal/src/main/java/com/app/controller/UserController.java
+++ Code/Agricultural-Portal/src/main/java/com/app/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ import com.app.service.IUserService;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController {
 
 	@Autowired
