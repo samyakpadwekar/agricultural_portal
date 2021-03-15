fb6ac128dc6abaa91eaf7f20e7a8986e7c155f0c
vikas-patel-06 <3471+vikas-patel-06@users.noreply.github.com>
2021-03-15 23:38:34 +0530

    SellerController File updated by : VIKAS+PANKAJ

--- Code/Agricultural-Portal/src/main/java/com/app/controller/SellerController.java
+++ Code/Agricultural-Portal/src/main/java/com/app/controller/SellerController.java
@@ -5,6 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,11 +19,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.app.dto.LoginRequest;
+import com.app.config.JwtTokenUtil;
 import com.app.dto.ProductDTO;
 import com.app.dto.ProductList;
 import com.app.dto.RestockProductDTO;
 import com.app.dto.SellerDTO;
+import com.app.dto.SellerSignupRequest;
+import com.app.model.JwtRequest;
+import com.app.model.JwtResponse;
 import com.app.pojos.Product;
 import com.app.pojos.Seller;
 import com.app.service.ISellerService;
@@ -28,26 +36,47 @@ import com.app.service.ISellerService;
 public class SellerController {
 
 	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+
+	@Autowired
 	private ISellerService sellerService;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
 	public SellerController() {
 		// TODO Auto-generated constructor stub
 		System.out.println("in ctor of seller controller");
 	}
-	
-	@PostMapping("/signup")
-	public ResponseEntity<?> sellerSignup(@RequestBody SellerDTO s) {
-		System.out.println("in seller signup "+s);
-		return ResponseEntity.ok(sellerService.sellerSignup(s));
-	}
-		
-	
-	@PostMapping("/login")
-	public ResponseEntity<?> sellerLogin(@RequestBody LoginRequest login) {
-		System.out.println("in seller login");
 
-		return new ResponseEntity<>(sellerService.authenticateSeller(login.getEmail(), login.getPassword()),
-				HttpStatus.OK);
+	@PostMapping("/register")
+	public ResponseEntity<?> saveSeller(@RequestBody SellerSignupRequest seller) {
+		System.out.println("in seller signup " + seller);
+
+		return ResponseEntity.ok(sellerService.saveSeller(seller));
+	}
+
+	@PostMapping("/authenticate")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+		System.out.println("above authenticate " + authenticationRequest);
+//		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+		System.out.println("bellow authenticate " + authenticationRequest);
+		final UserDetails userDetails = sellerService.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String token = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new JwtResponse(token));
+	}
+
+	private void authenticate(String username, String password) throws Exception {
+		try {
+			System.out.println("In line 80 of authenticate");
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
 	}
 
 	@GetMapping("/list-by-product")
@@ -64,7 +93,7 @@ public class SellerController {
 			return new ResponseEntity<ProductList>(new ProductList(productList), HttpStatus.NO_CONTENT);
 		return new ResponseEntity<ProductList>(new ProductList(productList), HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/inventory-report/{sellerId}")
 	public ResponseEntity<?> inventoryReport(@PathVariable Integer sellerId) {
 		System.out.println("in inventory report of seller");
@@ -80,16 +109,16 @@ public class SellerController {
 		System.out.println("in add category");
 		return new ResponseEntity<>(sellerService.addProduct(productDTO, sellerId, catId), HttpStatus.CREATED);
 	}
-	
+
 	@PutMapping("/edit-product")
 	public ResponseEntity<?> editProduct(@RequestBody ProductDTO productDTO) {
-		System.out.println("in edit product"); 
-		return new ResponseEntity<>(sellerService.editProduct(productDTO) , HttpStatus.OK);
+		System.out.println("in edit product");
+		return new ResponseEntity<>(sellerService.editProduct(productDTO), HttpStatus.OK);
 	}
-	
-	//manage inventory
+
+	// manage inventory
 	@PutMapping("/restock")
-	public ResponseEntity<?> restockProduct(@RequestBody RestockProductDTO restockProductDTO){
+	public ResponseEntity<?> restockProduct(@RequestBody RestockProductDTO restockProductDTO) {
 		System.out.println("in restock product");
 		return new ResponseEntity<>(sellerService.restockProduct(restockProductDTO), HttpStatus.OK);
 	}
