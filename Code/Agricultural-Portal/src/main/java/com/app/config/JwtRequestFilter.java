719fbca852794c78c49a7032094e18cf3566f802
43063_chinmay <chinmay.ohol@gmail.com>
2021-03-16 16:59:03 +0530

    Changed for postman setting

--- Code/Agricultural-Portal/src/main/java/com/app/config/JwtRequestFilter.java
+++ Code/Agricultural-Portal/src/main/java/com/app/config/JwtRequestFilter.java
@@ -39,9 +39,9 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 		String jwtToken = null;
 		// JWT Token is in the form "Bearer token". Remove Bearer word and get
 		// only the Token
-		if (requestTokenHeader != null  && requestTokenHeader.startsWith("Agricultural ") ) {
+		if (requestTokenHeader != null  && requestTokenHeader.startsWith("Bearer ") ) {
 			System.out.println("Token : "+requestTokenHeader);
-			jwtToken = requestTokenHeader.substring(13);
+			jwtToken = requestTokenHeader.substring(7);
 			System.out.println("Token : "+jwtToken);
 			
 			try {
