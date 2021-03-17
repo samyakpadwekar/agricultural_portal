205e0e8ff821e0e97707eeb8ac276946a1f72da8
Pankaj Chavan <chavanpankaj1996@gmail.com>
2021-03-17 22:19:48 +0530

    seller edit profile and seller signup screen created

---
+++ Frontend-Code/src/components/EditProfile/EditSellerPersonalInfo.js
@@ -0,0 +1,100 @@
+const EditSellerPersonalInfo = () => {
+  return (
+    <div className="container">
+      <h3>Edit Profile</h3>
+      <table className="table table-borderless ">
+        <tbody>
+          <tr>
+            <td colSpan="2">
+              <div className="row">
+                <div className="form-group">
+                  <label className="col-md-3 control-label">Username:</label>
+                  <div className="col-md-12">
+                    <input
+                      className="form-control"
+                      type="text"
+                      value=""
+                      readOnly="true"
+                    />
+                  </div>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="row">
+                <div className="form-group">
+                  <label className="col-lg-3 control-label">First name:</label>
+                  <div className="col-lg-12">
+                    <input className="form-control" type="text" />
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <div className="form-group">
+                <label className="col-lg-3 control-label">Last name:</label>
+                <div className="col-lg-12">
+                  <input className="form-control" type="text" />
+                </div>
+              </div>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <div className="form-group">
+                <label className="col-lg-3 control-label">Mobile No:</label>
+                <div className="col-lg-12">
+                  <input className="form-control" type="text" />
+                </div>
+              </div>
+            </td>
+            <td>
+              <div className="form-group">
+                <label className="col-lg-3 control-label">Email:</label>
+                <div className="col-lg-12">
+                  <input className="form-control" type="text" readOnly="true" />
+                </div>
+              </div>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <div className="form-group">
+                <label className="col-lg-3 control-label">PAN No :</label>
+                <div className="col-lg-12">
+                  <input className="form-control" type="text" />
+                </div>
+              </div>
+            </td>
+            <td>
+              <div className="form-group">
+                <label className="col-lg-3 control-label">AAdhar No :</label>
+                <div className="col-lg-12">
+                  <input className="form-control" type="text" />
+                </div>
+              </div>
+            </td>
+          </tr>
+
+          <tr>
+            <td colSpan="2">
+              <div className="row">
+                <div className="form-group">
+                  <label className="col-md-3 control-label">GSTIN :</label>
+                  <div className="col-md-12">
+                    <input className="form-control" type="text" value="" />
+                  </div>
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+export default EditSellerPersonalInfo
---
+++ Frontend-Code/src/screens/SellerSignup.js
@@ -0,0 +1,73 @@
+import { Link } from 'react-router-dom'
+import Header from '../components/Header'
+
+import '../styles/App.css'
+
+const SellerSignupScreen = () => {
+  return (
+    <div className="container">
+      <Header title="Signup" />
+      <div className="row">
+        <div className="col-mb-8 mx-auto">
+          <div className="sellerSignup">
+            <div className="form-horizontal">
+              <div className="mb-3">
+                <label className="form-label">User Name</label>
+                <input className="form-control"></input>
+              </div>
+
+              <div className="mb-3">
+                <label className="form-label">First Name</label>
+                <input className="form-control"></input>
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Last Name</label>
+                <input className="form-control"></input>
+              </div>
+
+              <div className="mb-3">
+                <label className="form-label">Buiseness Name</label>
+                <input className="form-control"></input>
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Mobile Number</label>
+                <input
+                  type="phone"
+                  className="form-control"
+                  placeholder="Mobile"
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="test@test.com"
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="*****"></input>
+              </div>
+              <div className="mb-3">
+                <label className="form-label">GSTIN</label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="mb-3">
+                <button className="btn btn-success">Signup</button>
+                <div className="float-end">
+                  Existing user? <Link to="/signin">Signin here</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SellerSignupScreen
