c9b0e223cf0e0dc8040d8ff27a2f5f6f287f3a42
Samyak Padwekar <samyaksam007@gmail.com>
2021-03-18 13:00:38 +0530

    all user about and homescreen front-end

---
+++ Frontend-Code/src/screens/users/ProductScreen.js
@@ -0,0 +1,11 @@
+import Header from '../component/Header'
+
+const ProductScreen = () => {
+  return (
+    <div>
+      <Header title="Product" subtitle="This is product page" />
+    </div>
+  )
+}
+
+export default ProductScreen
