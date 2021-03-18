8aa1ad3730fdadcc2107866ca97d4f5271e0133e
Samyak Padwekar <samyaksam007@gmail.com>
2021-03-18 12:48:39 +0530

    Header file updated

--- Frontend-Code/src/components/Header.js
+++ Frontend-Code/src/components/Header.js
@@ -1,15 +1,14 @@
 const Header = (props) => {
   return (
-    <div className="header">
-      <h1>{props.title}</h1>
-      {props.subtitle.length > 0 && <h2>{props.subtitle}</h2>}
+    <div>
+      <h1 className="title">{props.title}</h1>
+      {/* {props.subtitle.length > 0 && <h2>{props.subtitle}</h2>} */}
     </div>
   )
 }
 
 Header.defaultProps = {
   title: '',
-  subtitle: '',
 }
 
 export default Header
