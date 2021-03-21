57a332741b09f1e53dcd7b23fa1f8168e16405fb
vikas-patel-06 <3471+vikas-patel-06@users.noreply.github.com>
2021-03-21 20:05:29 +0530

    CartScreen Connected

--- Frontend-Code/src/screens/buyers/CartScreen.js
+++ Frontend-Code/src/screens/buyers/CartScreen.js
@@ -1,9 +1,18 @@
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getCartItems } from '../../actions/userActions'
+import store from '../../store'
 import '../../styles/App.css'
 const CartScreen = (props) => {
-  const onUpdateCart = () => {
-    props.history.push('/user/my-cart')
-  }
-  const testData = [{}]
+  const [subTotal, setSubTotal] = useState(0)
+  const dispatch = useDispatch()
+  const cartItemList = useSelector((store) => store.userCartItemsList)
+  const { error, response, loading } = cartItemList
+  useEffect(() => {
+    dispatch(getCartItems(7))
+  }, [])
+  useEffect(() => {}, [error, response, loading])
+
   return (
     <>
       <div className="container">
@@ -11,76 +20,84 @@ const CartScreen = (props) => {
           <div className="container-fluid">
             <div className="row">
               <div className="col-lg-8">
-                <table className="table table-bordered">
+                <table
+                  onLoad={() => {
+                    setSubTotal(response.totalCost)
+                  }}
+                  className="table table-bordered">
                   <thead className="thead-dark">
                     <tr>
                       <th>Product</th>
                       <th>Price(INR)</th>
                       <th>Quantity</th>
                       <th>Discount</th>
-                      <th>Total</th>
                       <th>Remove</th>
                     </tr>
                   </thead>
                   <tbody className="align-middle">
-                    {testData.map((cartItem) => {
-                      return (
-                        <>
-                          <tr>
-                            <td>
-                              <div className="img">
-                                <a href="#">
-                                  <img
-                                    style={{ width: 100, height: 100 }}
-                                    src="/images/product-1.jpg"
-                                    alt="Image"
+                    {response &&
+                      response.cartItems &&
+                      response.cartItems.length > 0 &&
+                      response.cartItems.map((item) => {
+                        return (
+                          <>
+                            <tr>
+                              <td>
+                                <div className="img">
+                                  <a href="#">
+                                    <img
+                                      style={{ width: 100, height: 100 }}
+                                      src="/images/product-1.jpg"
+                                      alt="Image"
+                                    />
+                                  </a>
+                                  <p>
+                                    {item.product.productCatalogue.productName}
+                                  </p>
+                                </div>
+                              </td>
+                              <td>{item.product.price}</td>
+                              <td>
+                                <div className="row justify-content-center">
+                                  <button
+                                    type="button"
+                                    className="btn-minus col-md-2">
+                                    <img
+                                      src="/images/dash.svg"
+                                      className="bi bi-plus"
+                                    />
+                                  </button>
+                                  <input
+                                    className="col-md-4"
+                                    type="text"
+                                    defaultValue={item.quantity}
                                   />
-                                </a>
-                                <p>A-I Blower type Pulvalizer </p>
-                              </div>
-                            </td>
-                            <td>99/-</td>
-                            <td>
-                              <div className="row justify-content-center">
+                                  <button
+                                    type="button"
+                                    className="btn-plus col-md-2">
+                                    <img
+                                      src="/images/plus.svg"
+                                      className="bi bi-plus"
+                                    />
+                                  </button>
+                                </div>
+                              </td>
+                              <td>{item.product.discount}</td>
+
+                              <td>
                                 <button
-                                  type="button"
-                                  className="btn-minus col-md-2">
+                                  className="btn-trash btn-light"
+                                  type="button">
                                   <img
-                                    src="/images/dash.svg"
-                                    className="bi bi-plus"
+                                    src="/images/trash.svg"
+                                    className="bi bi-trash"
                                   />
                                 </button>
-                                <input
-                                  className="col-md-4"
-                                  type="text"
-                                  defaultValue={1}
-                                />
-                                <button
-                                  type="button"
-                                  className="btn-plus col-md-2">
-                                  <img
-                                    src="/images/plus.svg"
-                                    className="bi bi-plus"
-                                  />
-                                </button>
-                              </div>
-                            </td>
-                            <td>10</td>
-                            <td>$99</td>
-                            <td>
-                              <button
-                                className="btn-trash btn-light"
-                                type="button">
-                                <img
-                                  src="/images/trash.svg"
-                                  className="bi bi-trash"
-                                />
-                              </button>
-                            </td>
-                          </tr>
-                        </>
-                      )
-                    })}
+                              </td>
+                            </tr>
+                          </>
+                        )
+                      })}
                   </tbody>
                 </table>
               </div>
@@ -91,13 +108,13 @@ const CartScreen = (props) => {
                       <div className="cart-content">
                         <h1>Cart Summary</h1>
                         <p>
-                          Sub Total<span>$99</span>
+                          Sub Total<span>{subTotal}</span>
                         </p>
                         <p>
-                          Shipping Cost<span>$1</span>
+                          Shipping Cost<span>0.0 INR</span>
                         </p>
                         <h2>
-                          Grand Total<span>$100</span>
+                          Grand Total<span>{subTotal}</span>
                         </h2>
                       </div>
                       <div className="cart-btn">
