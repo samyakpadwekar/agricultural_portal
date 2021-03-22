48c6d15249699b36c1b0d34030bd597463cfbeda
43063_chinmay <chinmay.ohol@gmail.com>
2021-03-22 12:56:19 +0530

    seller : order list screen integrated

--- Frontend-Code/src/screens/sellers/OrdersListScreen.js
+++ Frontend-Code/src/screens/sellers/OrdersListScreen.js
@@ -1,44 +1,81 @@
 import Header from "../../components/Header";
+import axios from 'axios';
+import { useState, useEffect } from "react";
 import '../../styles/App.css'
 
 const OrdersListScreen = (props) => {
-  const onConfirmOrder = () => {
-    props.history.push("/seller/your-orders");
-  };
-
-  const onDispatchOrder = () => {
-    props.history.push("/seller/your-orders");
-  };
-
-  const onDeliveredOrder = () => {
-    props.history.push("/seller/your-orders");
-  };
-
-  const onCancelOrder = () => {
-    props.history.push("/seller/your-orders");
-  };
-
-  const onMoreDetails = () => {
-    props.history.push("/seller/more-details");
-  };
-
-  const orderList = [
+  
+  const initValue = [
     {
-      orderId: 1,
-      orderDate: "19-03-2021",
-      orderDetails: "Order details Order details Order details Order details ",
-      status: "RECEIVED",
-      buyerId:3,
-      address:"Pune, MH"
+      "orderId": 4,
+      "buyer": {
+          "userId": 1,
+          "userName": "customer6",
+          "firstName": "customer",
+          "lastName": "one",
+          "email": "cust1@gmail.com",
+          "mobileNo": "+9166666666666",
+          "regDate": "2021-03-16",
+          "role": "CUSTOMER",
+          "bankDetails": {
+              "ifscCode": "ICIC05523",
+              "accNo": "12345678922"
+          },
+          "addresses": {
+              "pinCode": "393002",
+              "line1": "line1",
+              "line2": "line2",
+              "landMark": "landmark1",
+              "city": "Ahmedabad",
+              "state": "Gujarat",
+              "country": "India"
+          }
+      },
+      "paymentType": "CASH",
+      "orderDate": "2021-03-18T22:42:39.728",
+      "shipDate": null,
+      "transactionStatus": "UNPAID",
+      "estmDelvDate": null,
+      "paymentDate": null,
+      "orderStatus": "RECEIVED",
+      "totalAmount": 1200.0,
+      "paidAmount": null
+  }
+  ]
+  const [orders, setOrders] = useState(initValue);
+  useEffect(() => {
+    getAllOrders();
+  },[]);
+  sessionStorage.setItem('sellerId',1);
+  const url = 'http://localhost:8080/order/orders-list/'+sessionStorage.getItem('sellerId');
+  const header = {
+    headers: {
+      'Content-Type': 'application/json',
     },
-    {
-      orderId: 1,
-      orderDate: "19-03-2021",
-      orderDetails: "Order details ",
-      status: "RECEIVED",
-      buyerId:2,
-    },
-  ];
+  }
+  const getAllOrders = () => {
+    axios
+    .get(url, header)
+    .then((response) => {
+      if(response.status == 204){
+        // setMsg("No products");
+      }
+      const allOrders = response.data.orderList;
+      setOrders(allOrders);
+    })
+    .catch((error) => {
+      alert("Servers down")
+      console.error(`Error: ${error}`)
+    }
+    )
+  }
+  
+  
+
+  const onMoreDetails = (o) => {
+    props.history.push("/seller/order-details", o);
+  };
+
   return (
     <div className="mx-auto">
       <div>
@@ -72,26 +109,42 @@ const OrdersListScreen = (props) => {
             </tr>
           </thead>
           <tbody>
-            {orderList.map((o) => {
+            {orders && orders.map((o) => {
               return (
                 <tr>
                   <td>{o.orderId}</td>
                   <td>{o.orderDate}</td>
                   <td>
-                    {o.orderDetails}
+                    
                     <div>
-                      <span>Buyer Id :{o.buyerId}</span><br/>
-                      <span>Buyer Address :{o.address}</span><br/>
-                      <a href=""
+                      <span>Buyer Id :{o.buyer.userId}</span><br/>
+                      <span>Address :</span><br/>
+                      <span>{o.buyer.addresses && o.buyer.addresses.line1},</span><br/>
+                      <span>{o.buyer.addresses && o.buyer.addresses.line2},</span><br/>
+                      <span>{o.buyer.addresses && o.buyer.addresses.landMark}</span><br/>
+                      <span>{o.buyer.addresses && o.buyer.addresses.city},</span>
+                      <span>{o.buyer.addresses && o.buyer.addresses.state}</span><br/>
+                      {/* <a href=""
                         className="float-end float-bottom"
-                        onClick={onMoreDetails}
+                        //onClick={onMoreDetails(o)}
                       >
                         More details >>
-                      </a>
+                      </a> */}
+                      
                     </div>
                   </td>
-                  <td>{o.status}</td>
+                  <td>{o.orderStatus}</td>
                   <td>
+                  <button
+                      className="btn btn-outline-secondary order-status-btn"
+                      onClick={(e) => {
+                        onMoreDetails(o)
+                      }}
+                    >
+                      More details
+                    </button>
+                  </td>
+                  {/* <td>
                     <button
                       className="btn btn-outline-secondary order-status-btn"
                       onClick={onConfirmOrder}
@@ -120,7 +173,7 @@ const OrdersListScreen = (props) => {
                       Cancel
                     </button>
                     
-                  </td>
+                  </td> */}
                 </tr>
               );
             })}
